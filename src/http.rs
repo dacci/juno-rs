20467080ef572808802d78733d28fc50075e1102
Shinya Tsuda <shinya@dacci.org>
2022-10-29 11:03:47 +0900

    Refactor HTTP session handler.

--- src/http.rs
+++ src/http.rs
@@ -10,7 +10,7 @@ use std::task;
 use tokio::net::TcpStream;
 use tracing::error;
 
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct Service {
     dialer: Arc<Dialer>,
 }
@@ -43,6 +43,7 @@ impl tower::Service<TcpStream> for Service {
     }
 }
 
+#[cfg_attr(test, derive(Default))]
 struct Session {
     dialer: Arc<Dialer>,
 }
@@ -51,6 +52,129 @@ impl Session {
     fn new(dialer: &Arc<Dialer>) -> Self {
         Self {
             dialer: Arc::clone(dialer),
+        }
+    }
+
+    fn handle_connect(
+        &self,
+        req: Request<Body>,
+    ) -> impl Future<Output = Result<Response<Body>, hyper::Error>> {
+        let res = if let Some(authority) = req.uri().authority() {
+            let addr = authority.to_string();
+            let dialer = Arc::clone(&self.dialer);
+            Ok((addr, dialer))
+        } else {
+            Err(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from("CONNECT must be to a socket address"))
+                .unwrap())
+        };
+
+        async move {
+            let (addr, dialer) = match res {
+                Ok(req) => req,
+                Err(res) => return Ok(res),
+            };
+
+            let mut server = match dialer.dial(addr).await {
+                Ok(server) => server,
+                Err(e) => {
+                    return Ok(Response::builder()
+                        .status(StatusCode::BAD_GATEWAY)
+                        .body(Body::from(e.to_string()))
+                        .unwrap());
+                }
+            };
+
+            tokio::task::spawn(async move {
+                match hyper::upgrade::on(req).await {
+                    Ok(mut client) => {
+                        let _ = tokio::io::copy_bidirectional(&mut client, &mut server).await;
+                    }
+                    Err(e) => {
+                        error!("Failed to upgrade: {e}");
+                    }
+                };
+            });
+
+            Ok(Response::new(Body::empty()))
+        }
+    }
+
+    #[allow(clippy::declare_interior_mutable_const)]
+    const PROXY_CONNECTION: HeaderName = HeaderName::from_static("proxy-connection");
+
+    fn transform_request<T>(&self, mut req: Request<T>) -> Request<T> {
+        *req.uri_mut() = req
+            .uri()
+            .path_and_query()
+            .map(Clone::clone)
+            .map(Into::into)
+            .unwrap_or_default();
+
+        let map = req.headers_mut();
+        map.remove(Self::PROXY_CONNECTION);
+        map.remove(PROXY_AUTHORIZATION);
+
+        req
+    }
+
+    fn handle_request(
+        &self,
+        req: Request<Body>,
+    ) -> impl Future<Output = Result<Response<Body>, hyper::Error>> {
+        let res = if let Some(authority) = req.uri().authority() {
+            let addr = format!(
+                "{}:{}",
+                authority.host(),
+                authority.port_u16().unwrap_or(80)
+            );
+            let dialer = Arc::clone(&self.dialer);
+            let req = self.transform_request(req);
+            Ok((addr, dialer, req))
+        } else {
+            Err(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::empty())
+                .unwrap())
+        };
+
+        async move {
+            let (addr, dialer, req) = match res {
+                Ok(req) => req,
+                Err(res) => return Ok(res),
+            };
+
+            let stream = match dialer.dial(addr).await {
+                Ok(stream) => stream,
+                Err(e) => {
+                    return Ok(Response::builder()
+                        .status(StatusCode::BAD_GATEWAY)
+                        .body(Body::from(e.to_string()))
+                        .unwrap());
+                }
+            };
+
+            match Builder::new()
+                .http1_preserve_header_case(true)
+                .http1_title_case_headers(true)
+                .handshake(stream)
+                .await
+            {
+                Ok((mut sender, conn)) => {
+                    tokio::task::spawn(async move {
+                        if let Err(err) = conn.await {
+                            error!("Connection failed: {:?}", err);
+                        }
+                    });
+
+                    sender.send_request(req).await
+                }
+                Err(e) => Ok(Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(Body::from(e.to_string()))
+                    .unwrap()),
+            }
         }
     }
 }
@@ -65,108 +189,12 @@ impl tower::Service<Request<Body>> for Session {
     }
 
     fn call(&mut self, req: Request<Body>) -> Self::Future {
-        let dialer = Arc::clone(&self.dialer);
-        proxy(req, dialer).boxed()
-    }
-}
-
-async fn proxy(req: Request<Body>, dialer: Arc<Dialer>) -> Result<Response<Body>, hyper::Error> {
-    if Method::CONNECT == req.method() {
-        let addr = if let Some(authority) = req.uri().authority() {
-            authority.to_string()
+        if Method::CONNECT == req.method() {
+            self.handle_connect(req).boxed()
         } else {
-            return Ok(Response::builder()
-                .status(StatusCode::BAD_REQUEST)
-                .body(Body::from("CONNECT must be to a socket address"))
-                .unwrap());
-        };
-
-        let mut server = match dialer.dial(addr).await {
-            Ok(server) => server,
-            Err(e) => {
-                return Ok(Response::builder()
-                    .status(StatusCode::BAD_GATEWAY)
-                    .body(Body::from(e.to_string()))
-                    .unwrap())
-            }
-        };
-
-        tokio::task::spawn(async move {
-            match hyper::upgrade::on(req).await {
-                Ok(mut client) => {
-                    let _ = tokio::io::copy_bidirectional(&mut client, &mut server).await;
-                }
-                Err(e) => {
-                    error!("Could not upgrade: {e}");
-                }
-            };
-        });
-
-        Ok(Response::new(Body::empty()))
-    } else {
-        let addr = if let Some(authority) = req.uri().authority() {
-            format!(
-                "{}:{}",
-                authority.host(),
-                authority.port_u16().unwrap_or(80)
-            )
-        } else {
-            return Ok(Response::builder()
-                .status(StatusCode::BAD_REQUEST)
-                .body(Body::empty())
-                .unwrap());
-        };
-
-        let stream = match TcpStream::connect(addr).await {
-            Ok(stream) => stream,
-            Err(e) => {
-                return Ok(Response::builder()
-                    .status(StatusCode::BAD_GATEWAY)
-                    .body(Body::from(e.to_string()))
-                    .unwrap());
-            }
-        };
-
-        match Builder::new()
-            .http1_preserve_header_case(true)
-            .http1_title_case_headers(true)
-            .handshake(stream)
-            .await
-        {
-            Ok((mut sender, conn)) => {
-                tokio::task::spawn(async move {
-                    if let Err(err) = conn.await {
-                        error!("Connection failed: {:?}", err);
-                    }
-                });
-
-                let req = transform_request(req);
-                sender.send_request(req).await
-            }
-            Err(e) => Ok(Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(Body::from(e.to_string()))
-                .unwrap()),
+            self.handle_request(req).boxed()
         }
     }
-}
-
-#[allow(clippy::declare_interior_mutable_const)]
-const PROXY_CONNECTION: HeaderName = HeaderName::from_static("proxy-connection");
-
-fn transform_request<T>(mut req: Request<T>) -> Request<T> {
-    *req.uri_mut() = req
-        .uri()
-        .path_and_query()
-        .map(Clone::clone)
-        .map(Into::into)
-        .unwrap_or_default();
-
-    let map = req.headers_mut();
-    map.remove(PROXY_CONNECTION);
-    map.remove(PROXY_AUTHORIZATION);
-
-    req
 }
 
 #[cfg(test)]
@@ -182,7 +210,7 @@ mod tests {
             .body(())
             .unwrap();
 
-        let req = transform_request(req);
+        let req = Session::default().transform_request(req);
         assert_eq!(req.uri(), "/index.html");
         assert!(!req.headers().contains_key("Proxy-Connection"));
     }
